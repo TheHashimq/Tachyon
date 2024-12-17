2881440f8c6e2ce12871c4fe84ef4b8e6139c590
thehashimq <hashimq905@gmail.com>
2024-12-17 18:49:59 +0530

    hmm

---
+++ build/CMakeFiles/tachyon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tachyon.dir/src/main.cpp.o"
+  "CMakeFiles/tachyon.dir/src/main.cpp.o.d"
+  "tachyon"
+  "tachyon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tachyon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
